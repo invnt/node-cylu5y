2134e9718b73402c3c04bea0ef0a24f9fa9d865b
invnt <logan@invnt.co.za>
2022-01-25 21:44:08 +0200

    3 files modified

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 // run `node index.js` in the terminal
 
-const express = require('express');
+const app = require('express')();
 const bodyParser = require('body-parser');
-const app = express();
+const multer = require('multer'); // v1.0.5
+const upload = multer(); // for parsing multipart/form-data
 
-app.get('/', function (req, res) {
-  res.send('GET request to homepage');
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+app.get('/url', (req, res, next) => {
+  res.json(['Tony', 'Lisa', 'Michael', 'Ginger', 'Food']);
 });
 
 app.listen(80);
